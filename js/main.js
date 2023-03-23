5c1572c9cf9e829ee776724ad9a04dfefc118037
Andre Oliveira <122166590+oliveira-96@users.noreply.github.com>
2023-03-22 21:06:12 -0300

    1

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$('.slider-principal').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
